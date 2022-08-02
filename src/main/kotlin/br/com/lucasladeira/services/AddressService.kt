42f56b2113a635be05541f8ae2aa5eb8c0d3f065
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-08-02 16:48:56 -0300

    feature/AddressDTO

--- src/main/kotlin/br/com/lucasladeira/services/AddressService.kt
+++ src/main/kotlin/br/com/lucasladeira/services/AddressService.kt
@@ -1,11 +1,12 @@
 package br.com.lucasladeira.services
 
+import br.com.lucasladeira.dto.AddressDTO
 import br.com.lucasladeira.entities.Address
 
 interface AddressService {
-    fun getAddressByCep(cep: String): Address?
+    fun getAddressByCep(cep: String): AddressDTO?
     fun getAddressByStreetName(
         stateAbbreviation: String,
         cityName: String,
-        streetName: String): List<Address>?
+        streetName: String): List<AddressDTO>?
 }
--- src/main/kotlin/br/com/lucasladeira/services/AddressServiceImpl.kt
+++ src/main/kotlin/br/com/lucasladeira/services/AddressServiceImpl.kt
@@ -1,31 +1,40 @@
 package br.com.lucasladeira.services
 
 import br.com.lucasladeira.config.RetrofitConfig
-import br.com.lucasladeira.entities.Address
+import br.com.lucasladeira.dto.AddressDTO
+import br.com.lucasladeira.exceptions.EntityNotFoundException
+import org.modelmapper.ModelMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import retrofit2.create
 
 @Service
 class AddressServiceImpl: AddressService {
 
     @Autowired
     lateinit var retrofitConfig: RetrofitConfig
+    @Autowired
+    lateinit var mapper: ModelMapper
     private val BASE_URL = "https://viacep.com.br"
+    private val logger = LoggerFactory.getLogger(AddressServiceImpl::class.java)
 
-    override fun getAddressByCep(cep: String): Address?{
+    override fun getAddressByCep(cep: String): AddressDTO?{
         val apiService = retrofitConfig
             .retrofit(BASE_URL)
             .create(ViaCepService::class.java)
         val apiCall = apiService.getAddressByCep(cep)
-        return apiCall.execute().body()
+        val address = apiCall.execute().body()
+        if (address == null) throw EntityNotFoundException("Endereço não encontrado!", "000")
+        return mapper.map(address, AddressDTO::class.java)
     }
 
-    override fun getAddressByStreetName(stateAbbreviation: String, cityName: String, streetName: String): List<Address>? {
+    override fun getAddressByStreetName(stateAbbreviation: String, cityName: String, streetName: String): List<AddressDTO>? {
         val apiService = retrofitConfig
             .retrofit(BASE_URL)
             .create(ViaCepService::class.java)
         val apiCall = apiService.getAddressByStreetName(stateAbbreviation, cityName, streetName)
-        return apiCall.execute().body()
+        return apiCall.execute()
+            .body()!!
+            .map { address -> mapper.map(address, AddressDTO::class.java) }
     }
 }
