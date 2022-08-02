3de4612d36a11cf9fc14800161ee6cf52c6e2c5e
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-08-02 12:11:28 -0300

    Initial Commit

---
+++ src/main/kotlin/br/com/lucasladeira/ConsultaCepApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.lucasladeira
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsultaCepApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsultaCepApiApplication>(*args)
+}
